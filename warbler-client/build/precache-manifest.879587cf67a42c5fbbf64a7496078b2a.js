a845ca5a8d4c5213de5a7f7da977df6dc5746500
Jan Magayanes <janine@chancen-eg.de>
2020-10-05 22:20:17 +0200

    first commit

---
+++ warbler-client/build/precache-manifest.879587cf67a42c5fbbf64a7496078b2a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9aff8b55cb8fbaad18879c7e920c365",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9968418856d2af06c550",
+    "url": "/static/css/main.95075812.chunk.css"
+  },
+  {
+    "revision": "1ca0e04a7e844819d325",
+    "url": "/static/js/2.7b2da5d7.chunk.js"
+  },
+  {
+    "revision": "320addaff3e37295a01ab28fa4b66477",
+    "url": "/static/js/2.7b2da5d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9968418856d2af06c550",
+    "url": "/static/js/main.2277d2b3.chunk.js"
+  },
+  {
+    "revision": "3e1268a0bcc68bdcd735",
+    "url": "/static/js/runtime-main.fa13f56a.js"
+  },
+  {
+    "revision": "43b32b1f349b290d408ae00e183879e9",
+    "url": "/static/media/signed-out-home.43b32b1f.jpg"
+  },
+  {
+    "revision": "25398cf74efabfc8f446dfcb6c2a6338",
+    "url": "/static/media/warbler-logo.25398cf7.png"
+  }
+]);
